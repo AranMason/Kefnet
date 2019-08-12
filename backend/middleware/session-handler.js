16a5ac1745ea8b43feecf31108629d9aa15f7c9c
Aran Mason <aran.j.mason@gmail.com>
2019-08-12 22:00:17 +1200

    isLoggedIn does nothing now. Should update?

--- backend/middleware/session-handler.js
+++ backend/middleware/session-handler.js
@@ -3,9 +3,9 @@ var isLoggedIn = (req, res, next) => {
 
     // req.session.reload((err) => {
 
-        console.log("Session Id: ", req.sessionID);
-        console.log("Session: ", req.session);
-        console.log("Cookie: ", req.cookies);
+        // console.log("Session Id: ", req.sessionID);
+        // console.log("Session: ", req.session);
+        // console.log("Cookie: ", req.cookies);
 
         // if(err){
         //     console.log("Error: ", err)
