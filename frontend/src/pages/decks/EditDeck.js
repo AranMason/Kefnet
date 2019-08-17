cbe204d2fe4f35ce89b78c9b4775a2c36bbeebd6
Aran Mason <aran.j.mason@gmail.com>
2019-08-17 14:52:43 +1200

    Created baseline edit deck page

--- frontend/src/pages/decks/EditDeck.js
+++ frontend/src/pages/decks/EditDeck.js
@@ -1,20 +1,100 @@
 import React from 'react';
 
+import Loading from '../../template/Loading';
+
+import { Form } from 'react-bootstrap';
+
 class DeckPage extends React.Component {
+
+	constructor(props){
+		super(props)
+
+		this.state = {
+			isLoading: true,
+			deck_id: props.location.params.deck_id,
+			name: '',
+			external: {
+				url: '',
+				provider: null,
+				id: null
+			},
+			// provider: 'None'
+		}
+
+		this.handleChange = this.handleChange.bind(this);
+		this.renderTitle = this.renderTitle.bind(this);
+	}
+
+	componentWillMount(){
+		if(this.state.deck_id){
+			//Load deck
+
+			this.setState({
+				isLoading: false
+			})
+		}
+		else {
+			this.setState({
+				isLoading: false
+			})
+		}
+	}
+
+	handleChange(e){
+
+		if(e.target.name === "name"){
+			this.setState({
+				name: e.target.value
+			})
+		}
+		else if(e.target.name === "url"){
+			this.setState({
+				external: {
+					url: e.target.value
+					//TODO Extract provider?
+					//TODO Extract provider deck id
+				}
+			})
+		}
+
+		this.setState
+	}
+
+	renderTitle(){
+		if(this.state.deck_id){
+			return (
+				<h2>
+					Editing {this.state.name}
+				</h2>
+			)
+		}
+		else {
+			return (
+				<h2>
+					New Deck
+				</h2>
+			)
+		}
+	}
+
 	render() {
 
-		if(this.props.location.params.deck_id){
-			return (
-				<p>
-					{`Edit deck ${this.props.location.params.deck_id}`}
-				</p>
-			)
+		if(this.state.isLoading){
+			return (<Loading />)
 		}
 
 		return (
-			<p>
-				Create Deck
-			</p>
+			<div>
+				{this.renderTitle()}
+				<Form>
+					<input name="name" type="text" onChange={this.handleChange} value={this.state.name} placeholder="Deck name"></input>
+
+					<input name="url" type="text" onChange={this.handleChange} value={this.state.external.url} placeholder="Deck link" />
+				</Form>
+				<div className="Deck-provider">
+					{this.state.external.provider}
+				</div>	
+			</div>
 		)
 	}
 }
