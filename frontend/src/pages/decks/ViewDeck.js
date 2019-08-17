2a942e4466bfe5a7ca269f5051fa1cc5107c907e
Aran Mason <aran.j.mason@gmail.com>
2019-08-17 15:23:30 +1200

    First pass at View Deck

--- frontend/src/pages/decks/ViewDeck.js
+++ frontend/src/pages/decks/ViewDeck.js
@@ -1,7 +1,50 @@
 import React from 'react';
 
+import Loading from '../../components/Loading';
+
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
 class DeckPage extends React.Component {
+
+	constructor(props){
+		super(props)
+
+		this.state = {
+			isLoading: true,
+			redirect: false,
+			deck_id: this.props.location.props && this.props.location.params.deck_id,
+			deck: null
+		}
+	}
+
+	componentWillMount(){
+		axios.get(`/deck/${this.state.deck_id}`).then(res => {
+			this.setState({
+				isLoading: false,
+				deck: res.body
+			})
+		}).catch(err => {
+			console.err(err);
+			this.setState({
+				redirect: '/deck'
+			})
+		})
+	}
 	render() {
+
+		if(this.state.isLoading || !this.state.deck){
+			return (
+				<Loading />
+			)
+		}
+
+		if(this.state.redirect){
+			return (
+				<Redirect to={this.state.redirect} />
+			)
+		}
+		
 		return (
 			<div>
 				<p>
