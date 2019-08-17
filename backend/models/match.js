1ee0d802b77970a3fafb6159f610db54bb5d1749
Aran Mason <aran.j.mason@gmail.com>
2019-08-17 20:38:03 +1200

    Fixed reporting for database creation

--- backend/models/match.js
+++ backend/models/match.js
@@ -31,7 +31,7 @@ var Match = sequelize.define('match', {
 
 // create all the defined tables in the specified database.
 sequelize.sync()
-    .then(() => console.log('decks table has been successfully created, if one doesn\'t exist'))
+    .then(() => console.log('matches table has been successfully created, if one doesn\'t exist'))
     .catch(error => console.log('This error occured', error));
 
 // export User model for use in other files.
