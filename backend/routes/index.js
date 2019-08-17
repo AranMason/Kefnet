a90fa37a3d2d3d025fd0bee86b72f6a22b9249dc
Aran Mason <aran.j.mason@gmail.com>
2019-08-17 20:38:39 +1200

    Fixed statuscode/send layout

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.statusCode(404);
-  res.send("Could not find resource, check your URL")
+  // res.statusCode(404);
+  res.status(404).send("Could not find resource, check your URL")
 });
 
 // route for user logout
