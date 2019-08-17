5d861c7413b640427466f5f1449856c3530cf08e
Aran Mason <aran.j.mason@gmail.com>
2019-08-17 20:41:17 +1200

    First pass at Dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,13 +1,42 @@
 import React from 'react';
 
+import {} from 'react-bootstrap';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 class Dashboard extends React.Component {
 	render(){
+
+		if(!this.props.isLoggedIn){
+			return (
+				<Redirect to="/" />
+			)
+		}
+
 		return (
-			<p>
-				User Dashboard
-			</p>
+			<div>
+				<Link to="/deck/add">
+					Add Deck
+				</Link>
+				<Link to="/matchs/create">
+					Register New Match
+				</Link>
+				TO DO List:
+				<ul>
+					<li>List Decks that you have</li>
+					<li>List Matches that you have been in recently</li>
+					<li>Give you some stats about your games</li>
+				</ul>
+			</div>
 		)
 	}
 }
 
-export default Dashboard;
+function mapStateToProps(state, ownProps) {
+	return {
+		isLoggedIn: state.login.isLoggedIn,
+		user: state.login.user
+	}
+}
+
+export default connect(mapStateToProps)(Dashboard);
