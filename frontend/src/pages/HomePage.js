cb47ddd0ac41bf1437087309a32ea03cffb6b402
Aran Mason <aran.j.mason@gmail.com>
2019-08-20 19:10:13 +1200

    Added todo tracking to homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,9 +3,30 @@ import React from 'react'
 class HomePage extends React.Component {
     render(){
         return (
-            <p>
-                HomePage
-            </p>
+            <div>
+                <p>
+                    HomePage
+                </p>
+                TODO List:
+                <ul>
+                    <li>
+                        Build Homepage - Need ideas
+                    </li>
+                    <li>
+                        Build Matches
+                    </li>
+                    <li>
+                        Build User Dashboard
+                    </li>
+                    <li>
+                        Support Password Changes
+                    </li>
+                    <li>
+                        Support variant formats
+                    </li>
+                </ul>
+            </div>
+
         )
     }
 }
