1dd0cfe0a53faa1d9c84154d9d13f810bec5231a
Aran Mason <aran.j.mason@gmail.com>
2019-08-20 13:22:55 +1200

    Removed unused import

--- frontend/src/components/HeaderLogin.js
+++ frontend/src/components/HeaderLogin.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './HeaderLogin.css'
 
-import { Redirect, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
