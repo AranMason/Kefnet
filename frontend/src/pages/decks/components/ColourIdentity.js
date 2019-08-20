e239144c97a010205df6beb68c885484f7530ffe
Aran Mason <aran.j.mason@gmail.com>
2019-08-20 14:02:23 +1200

    Stubbed ColourIdentity object

---
+++ frontend/src/pages/decks/components/ColourIdentity.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ColourIdentity extends React.Component {
+
+    render(){
+        return (
+            <div>
+                {this.props.identity}
+            </div>
+            
+        )
+    }
+}
