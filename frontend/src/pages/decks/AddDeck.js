19e4fc6a306ef28cb8c733c1379387adb751bbe8
Aran Mason <aran.j.mason@gmail.com>
2019-08-21 14:12:37 +1200

    Now requires you to be logged in

--- frontend/src/pages/decks/AddDeck.js
+++ frontend/src/pages/decks/AddDeck.js
@@ -5,6 +5,9 @@ import Loading from '../../components/Loading';
 
 import axios from 'axios';
 
+import { connect } from 'react-redux';
+import { getLoginStatus } from '../../redux/actions/login';
+
 import { Form, Button, Alert, Col } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 import FormatDropdown from './components/FormatDropdown';
@@ -12,7 +15,7 @@ import ColourIdentity from './components/ColourIdentity';
 
 class AddDeck extends React.Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props)
 
 		this.state = {
@@ -38,14 +41,14 @@ class AddDeck extends React.Component {
 		// this.renderTitle = this.renderTitle.bind(this);
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		this.setState({
 			isLoading: false
 		})
 	}
 
-	handleChange(e){
-		if(e.target === "ColourIdentity"){
+	handleChange(e) {
+		if (e.target === "ColourIdentity") {
 			this.setState({
 				colour: {
 					...this.state.colour,
@@ -53,24 +56,24 @@ class AddDeck extends React.Component {
 				}
 			})
 		}
-		else if(e.target.name === "name"){
+		else if (e.target.name === "name") {
 			this.setState({
 				name: e.target.value
 			})
 		}
-		else if(e.target.name === "url"){
+		else if (e.target.name === "url") {
 			this.setState({
 				url: e.target.value
 			})
 		}
-		else if(e.target.name === "format"){
+		else if (e.target.name === "format") {
 			this.setState({
 				format: e.target.value
 			})
 		}
 	}
 
-	handleSubmit(e){
+	handleSubmit(e) {
 		e.preventDefault();
 		this.setState({
 			isLoading: true
@@ -112,9 +115,9 @@ class AddDeck extends React.Component {
 		})
 	}
 
-	renderAlert(){
+	renderAlert() {
 
-		if(this.state.alert){
+		if (this.state.alert) {
 			return (
 				<Alert variant={this.state.alert.type}>
 					{this.state.alert.message}
@@ -128,13 +131,19 @@ class AddDeck extends React.Component {
 
 	render() {
 
-		if(this.state.isLoading){
+		if (this.state.isLoading) {
 			return (<Loading />)
 		}
 
-		if(this.state.redirect){
+		if (this.state.redirect) {
 			return (
-				<Redirect to={this.state.redirect}/>
+				<Redirect to={this.state.redirect} />
+			)
+		}
+
+		if (!this.props.isLoggedIn) {
+			return (
+				<Redirect to={'/login?redirect=/deck/add'} />
 			)
 		}
 
@@ -147,20 +156,20 @@ class AddDeck extends React.Component {
 					{this.renderAlert()}
 					<Form.Group>
 						<Form.Label>Deck name</Form.Label>
-    					<Form.Control name="name" onChange={this.handleChange} value={this.state.name} type="text" placeholder="Deck name..." />
+						<Form.Control name="name" onChange={this.handleChange} value={this.state.name} type="text" placeholder="Deck name..." />
 					</Form.Group>
 
 					<Form.Group>
 						<Form.Label>Deck URL</Form.Label>
-    					<Form.Control name="url" onChange={this.handleChange} value={this.state.url} type="text" placeholder="Deck url..." />
+						<Form.Control name="url" onChange={this.handleChange} value={this.state.url} type="text" placeholder="Deck url..." />
 						<Form.Text className="text-muted">
-    					  Provide a link to your decklist, currently supporting: 
+							Provide a link to your decklist, currently supporting:
 						  <ul>
-							  <li>MTG Goldfish</li>
-							  <li>Tappedout</li>
-							  <li>Archidekt</li>
-						  </ul>
-    					</Form.Text>
+								<li>MTG Goldfish</li>
+								<li>Tappedout</li>
+								<li>Archidekt</li>
+							</ul>
+						</Form.Text>
 					</Form.Group>
 
 					<Form.Row>
@@ -170,7 +179,7 @@ class AddDeck extends React.Component {
 								value={this.state.colour} />
 						</Form.Group>
 						<Form.Group as={Col} md="8">
-							<FormatDropdown 
+							<FormatDropdown
 								name="format"
 								onChange={this.handleChange}
 								value={this.state.format} />
@@ -178,14 +187,29 @@ class AddDeck extends React.Component {
 					</Form.Row>
 
 					<Button variant="primary" type="submit">
-    Submit
-  </Button>
-					
+						Submit
+  					</Button>
+
 				</Form>
-					
+
 			</div>
 		)
 	}
 }
 
-export default AddDeck;
+const mapDispatchToProps = (dispatch, ownProps) => {
+	return {
+		loginStatus: () => {
+			dispatch(getLoginStatus())
+		}
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		isLoggedIn: state.login.isLoggedIn
+	}
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddDeck);
